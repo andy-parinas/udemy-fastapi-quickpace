656073cf6e282c1dde3ba20d9a23cc31db100764
Andy Parinas <andy@budgetpetproducts.com.au>
2022-12-21 20:20:30 +1000

    DB Models

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(256), nullable=True)
+    last_name = Column(String(256), nullable=True)
+    email = Column(String(256), index=True, nullable=False)
+    is_superuser = Column(Boolean, default=False)
+    recipes = relationship("Recipe", cascade="all,delete-orphan", back_populates="submitter", useList=True)
